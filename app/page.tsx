0940af2d41118e767548961fbd3dfaafb19b28c8
Ryo <r-tabata@lockup.jp>
2024-09-03 23:52:53 +0900

    画像の最適化

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 import styles from './page.module.css'
+import Image from 'next/image' //画像の読み込みにはImageコンポーネントを使用して、画像の最適化を行う
 
 export default function Home () {
   return (
     <section className={styles.top}>
       <div>
-        <h1 className={styles.title}>これはH1のテストです</h1>
-        <p className={styles.description}>これはpタグのテストです</p>
+        <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
       </div>
-      <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+        width={4000}
+        height={1200}
+        />
     </section>
   )
 }
